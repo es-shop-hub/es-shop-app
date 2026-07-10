68fbfce79090df8b958a2936b4937f582e4f6ec7
Es-Shop <contact.es.shop1@gmail.com>
2026-07-10 16:32:25 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "stockflow v7";
+const CACHE_NAME = "stockflow v3";
 const BASE_PATH =
   self.location.pathname.replace(/\/service-worker\.js$/, "") || "";
 const withBase = (path) => {
